c6585c306e87ead6593930c7420893fe2880e6da
הדר אורבך <orbachadar@gmail.com>
2025-11-25 15:41:06 +0200

    initial project and add mongo connection

---
+++ index.js
@@ -0,0 +1,24 @@
+try { require('dotenv').config({ path: '.env' }); } catch (e) {}
+const express = require("express");
+const connectDB = require("./config/db");
+
+const PORT = Number(process.env.PORT) ;
+const postRouter = require("./routers/post");
+const commentRouter = require("./routers/comment");
+
+
+const app = express();
+app.use(express.json());
+
+(async () => {
+  await connectDB();
+  
+  app.get("/", (req, res) => {
+    res.send("Server is up");
+  });
+
+//   app.use("/posts", postRouter);
+//   app.use("/comments", commentRouter);
+
+  app.listen(PORT, () => console.log("Server running on port 3000"));
+})();
