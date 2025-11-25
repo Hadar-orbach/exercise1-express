4545ef0aa0526aec1fa6d830383322203c91d93a
הדר אורבך <orbachadar@gmail.com>
2025-11-25 22:16:50 +0200

    add comment router

--- routers/post.js
+++ routers/post.js
@@ -2,12 +2,10 @@ const express = require("express");
 const router = express.Router();
 const postController = require("../controllers/postController");
 
-// your routes:
 router.post("/", postController.addPost);
 router.get("/", postController.getAllPosts);
 router.get("/:id", postController.getPostById);
 
-// partner routes:
 router.get("/filter/by-sender", postController.getPostsBySender);
 router.put("/:id", postController.updatePost);
 
