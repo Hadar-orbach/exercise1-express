a2f3a56ca1aec2e16504c85091969c6fbee6995e
meitarberko <meitar.be@gmail.com>
2025-11-25 22:01:53 +0200

    post controller: getPostsBySender, updatePost

--- controllers/post_controller.js
+++ controllers/post_controller.js
@@ -31,33 +31,27 @@ exports.getPostById = async (req, res) => {
   }
 };
 
-const Post = require("../models/postModel");
-
-exports.addPost = async (req, res) => {
+exports.getPostsBySender = async (req, res) => {
   try {
-    const post = await Post.create(req.body);
-    res.status(201).json(post);
-  } catch (err) {
-    res.status(400).json({ error: err.message });
-  }
-};
-
-exports.getAllPosts = async (req, res) => {
-  try {
-    const posts = await Post.find();
+    const posts = await Post.find({ sender: req.query.sender });
     res.json(posts);
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
 };
 
-exports.getPostById = async (req, res) => {
+exports.updatePost = async (req, res) => {
   try {
-    const post = await Post.findById(req.params.id);
+    const post = await Post.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+
     if (!post) return res.status(404).json({ error: "Post not found" });
+
     res.json(post);
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
 };
-
